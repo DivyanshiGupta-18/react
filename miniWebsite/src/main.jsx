0179cbb51c6c34cc2526d7a06ff20a1ac91dde2e
DivyanshiGupta-18 <guptadivyanshi756@gmail.com>
2025-02-22 23:18:02 +0530

    mini website

---
+++ miniWebsite/src/main.jsx
@@ -0,0 +1,32 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import About from './Components/About.jsx'
+import Contact from './Components/Contact.jsx'
+ import Home from './Components/Home.jsx'
+import User from './Components/User.jsx'
+import Github, { githubInfoLoader } from './Components/Github.jsx'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout />}>
+      <Route path='' element={<Home />} />
+      <Route path='about' element={<About />} />
+      <Route path='contact' element={<Contact />} />
+      <Route path='user/:userid' element={<User />} /> */}
+       <Route 
+      loader={githubInfoLoader}
+      path='github' 
+      element={<Github />} 
+       />
+    </Route>
+  )
+)
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+     <RouterProvider router={router} />
+  </StrictMode>,
+)
